4d76fb4e60035f839ad667975dec5e30891f46e9
Kyle Fuller <kyle@fuller.li>
2015-10-18 09:41:16 -0700

    [Template] Include tests for bundle/url initialiser

--- StencilTests/TemplateTests.swift
+++ StencilTests/TemplateTests.swift
@@ -9,4 +9,27 @@ class TemplateTests: XCTestCase {
     let result = try? template.render(context)
     XCTAssertEqual(result, "Hello World")
   }
+
+  func testTemplateNamedInBundle() {
+    let testBundle = NSBundle(forClass: self.dynamicType)
+    let template = try? Template(named: "test.html", inBundle: testBundle)
+    let context = Context(dictionary: [ "target": "Kyle" ])
+
+    XCTAssertNotNil(template)
+    let result = try? template!.render(context)
+    XCTAssertEqual(result, "Hello Kyle!")
+  }
+
+
+  func testTemplateWithNSURL() {
+    let testBundle = NSBundle(forClass: self.dynamicType)
+    let URL = testBundle.URLForResource("test", withExtension: "html")
+    XCTAssertNotNil(URL)
+    let template = try? Template(URL: URL!)
+    let context = Context(dictionary: [ "target": "Kyle" ])
+
+    XCTAssertNotNil(template)
+    let result = try? template!.render(context)
+    XCTAssertEqual(result, "Hello Kyle!")
+  }
 }
