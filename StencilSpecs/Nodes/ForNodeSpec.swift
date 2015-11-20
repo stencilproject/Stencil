cba1cbe3886e99fe7aece82f303e83fb0a5a4488
Niels de Hoog <njdehoog@gmail.com>
2015-11-20 15:07:36 +0100

    Updated specs for ForNode

--- StencilSpecs/Nodes/ForNodeSpec.swift
+++ StencilSpecs/Nodes/ForNodeSpec.swift
@@ -1,5 +1,6 @@
 import Spectre
 import Stencil
+import Foundation
 
 
 describe("ForNode") {
@@ -19,5 +20,25 @@ describe("ForNode") {
     let emptyNodes: [NodeType] = [TextNode(text: "empty")]
     let node = ForNode(variable: "emptyItems", loopVariable: "item", nodes: nodes, emptyNodes: emptyNodes)
     try expect(try node.render(context)) == "empty"
+  }
+
+  $0.it("renders a context variable of type Array<Any>") {
+    let any_context = Context(dictionary: [
+        "items": ([1, 2, 3] as [Any])
+      ])
+
+    let nodes: [NodeType] = [VariableNode(variable: "item")]
+    let node = ForNode(variable: "items", loopVariable: "item", nodes: nodes, emptyNodes: [])
+    try expect(try node.render(any_context)) == "123" 
+  }
+
+  $0.it("renders a context variable of type NSArray") {
+    let nsarray_context = Context(dictionary: [
+        "items": NSArray(array: [1, 2, 3])
+      ])
+
+    let nodes: [NodeType] = [VariableNode(variable: "item")]
+    let node = ForNode(variable: "items", loopVariable: "item", nodes: nodes, emptyNodes: [])
+    try expect(try node.render(nsarray_context)) == "123" 
   }
 }
