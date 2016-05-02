386ec5c780785daa235074960c12397c6232955f
Basem Emara <basememara@gmail.com>
2016-05-02 17:34:55 -0400

    Added Xcode and Carthage support.

---
+++ Stencil.h
@@ -0,0 +1,17 @@
+//
+//  Stencil.h
+//  Stencil
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stencil.
+FOUNDATION_EXPORT double StencilVersionNumber;
+
+//! Project version string for Stencil.
+FOUNDATION_EXPORT const unsigned char StencilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stencil/PublicHeader.h>
+
+
