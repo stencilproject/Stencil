d851a2903946aa0f095e0df04d241bb4b4c5ca72
Nick Griffith <nick.griffith@metova.com>
2018-03-02 14:11:27 -0600

    fixed syntax error

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
   name: "Stencil",
   products: [
     .library(name: "Stencil", targets: ["Stencil"])
-  ]
+  ],
   dependencies: [
     .package(url: "https://github.com/kylef/PathKit.git", .upToNextMinor(from: "0.9.0")),
     .package(url: "https://github.com/kylef/Spectre.git", .upToNextMinor(from: "0.8.0")),
