bb35be224c89fd6fcff35ffd8e939efee5244403
Nick Griffith <nhgrif@gmail.com>
2018-03-02 13:54:57 -0600

    fixing syntax to swift 4

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
   name: "Stencil",
   dependencies: [
-    .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 9),
-    .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 8),
+    .package(url: "https://github.com/kylef/PathKit.git", .upToNextMinor(from: "0.9.0")),
+    .package(url: "https://github.com/kylef/Spectre.git", .upToNextMinor(from: "0.8.0")),
   ]
 )
