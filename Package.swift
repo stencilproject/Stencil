d045f1045dd1859b34136ec4c196b9902f53535b
Neha <neha.oscprofessionals@gmail.com>
2021-09-22 14:33:29 +0530

    Update Package.swift
    update Spectre library version

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
-    .package(url: "https://github.com/kylef/Spectre.git", from: "0.9.0")
+    .package(url: "https://github.com/kylef/Spectre.git", from: "0.10.0")
   ],
   targets: [
     .target(name: "Stencil", dependencies: [
