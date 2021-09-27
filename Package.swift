ac8192136e8fecea8bb102da416f11640082b31f
Hesham Salman <iron-ham@github.com>
2021-09-27 14:57:23 -0400

    Update packages so as to successfully compile in Xcode 13

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ let package = Package(
     .library(name: "Stencil", targets: ["Stencil"])
   ],
   dependencies: [
-    .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
-    .package(url: "https://github.com/kylef/Spectre.git", from: "0.9.0")
+    .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.1"),
+    .package(url: "https://github.com/kylef/Spectre.git", from: "0.10.1")
   ],
   targets: [
     .target(name: "Stencil", dependencies: [
